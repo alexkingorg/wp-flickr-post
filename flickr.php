1ab3e3ad85c60c4cb72ec38a431b220238d6589c
Alex King <alex@alexking.org>
2011-09-13 00:43:02 -0600

    abstracted to allow different feed parsing

--- flickr.php
+++ flickr.php
@@ -8,20 +8,24 @@ Description: Create posts from Flickr images.
 @define('AKV3_FLICKR_CAT', '5');
 @define('AKV3_FLICKR_TIMEZONE', 'America/Denver');
 @define('AKV3_FLICKR_DRYRUN', false);
-
-function akv3_flickr_feeds() {
-// NOTE: these Flickr APIs update very slowsly and omit various images for reasons unknown - they are not to be trusted. 
-	return array(
-		'http://api.flickr.com/services/feeds/photos_public.gne?id=25977117@N00&tags=instagramapp&lang=en-us&format=json&nojsoncallback=1',
-		'http://api.flickr.com/services/feeds/photos_public.gne?id=25977117@N00&tags=toblog&lang=en-us&format=json&nojsoncallback=1',
-	);
-}
+@define('AKV3_FLICKR_API', 'json');
 
 function akv3_flickr_cron() {
-	$feeds = akv3_flickr_feeds();
+	set_time_limit(0);
+	switch (AKV3_FLICKR_API) {
+		case 'html':
+			$feeds = akv3_flickr_feeds_html();
+			$parse = 'akv3_flickr_process_feed_html';
+		break;
+		case 'json':
+			$feeds = akv3_flickr_feeds_json();
+			$parse = 'akv3_flickr_process_feed_json';
+		break;
+	}
 	if (is_array($feeds) && count($feeds)) {
 		foreach ($feeds as $feed) {
-			akv3_flickr_process_feed($feed);
+			$photos = $parse($feed);
+			akv3_flickr_insert_posts($photos);
 		}
 	}
 	if (AKV3_FLICKR_DRYRUN) {
@@ -34,12 +38,15 @@ function akv3_flickr_guid($guid) {
 	return $guid;
 }
 
-function akv3_flickr_process_feed($url) {
-	set_time_limit(0);
-	$tz = date_default_timezone_get();
-	date_default_timezone_set(AKV3_FLICKR_TIMEZONE);
-	global $wpdb;
+function akv3_flickr_feeds_json() {
+// NOTE: these Flickr APIs update very slowsly and omit various images for reasons unknown - they are not to be trusted. 
+	return array(
+		'http://api.flickr.com/services/feeds/photos_public.gne?id=25977117@N00&tags=instagramapp&lang=en-us&format=json&nojsoncallback=1',
+		'http://api.flickr.com/services/feeds/photos_public.gne?id=25977117@N00&tags=toblog&lang=en-us&format=json&nojsoncallback=1',
+	);
+}
 
+function akv3_flickr_process_feed_json($url) {
 // get JSON
 	$response = wp_remote_get($url);
 	if (is_wp_error($response)) {
@@ -50,56 +57,88 @@ function akv3_flickr_process_feed($url) {
 
 	$photos = array();
 	if (count($data->items)) {
-		foreach ($data->items as $item) {
-			$photos[akv3_flickr_guid($item->link)] = $item;
-		}
-// grab item hashes, look for existing items (use GUID)
-		$guids = array_keys($photos);
-		$existing = $wpdb->get_col("
-			SELECT guid
-			FROM $wpdb->posts
-			WHERE guid IN ('".implode("', '", array_map(array($wpdb, 'escape'), $guids))."')
-		");
-		if (!AKV3_FLICKR_DRYRUN) {
-			foreach ($existing as $guid) {
-				unset($photos[$guid]);
+		$tz = date_default_timezone_get();
+		date_default_timezone_set(AKV3_FLICKR_TIMEZONE);
+		foreach ($data->items as $photo) {
+			$guid = akv3_flickr_guid($photo->link);
+			$title = $photo->title;
+			if (empty($title)) {
+				$title = 'Untitled Photo';
 			}
+			$photos[$guid] = array(
+				'url' => str_replace('_m.jpg', '_b.jpg', $photo->media->m), // get the large size
+				'guid' => $guid,
+				'title' => $title,
+				'description' => $photo->description,
+				'date' => strtotime($photo->published), // timestamp
+				'tags' => $photo->tags,
+			);
+		}
+// restore timezone
+		date_default_timezone_set($tz);
+	}
+	return $photos;
+}
+
+function akv3_flickr_feeds_html() {
+	return array(
+		'http://www.flickr.com/photos/alexkingorg/tags/instagramapp/',
+		'http://www.flickr.com/photos/alexkingorg/tags/toblog/',
+	);
+}
+
+function akv3_flickr_process_feed_html($url) {
+	die('TODO');
+}
+
+function akv3_flickr_insert_posts($photos = array()) {
+/* format for inbound photo data
+$photo = array(
+	'url' => ,
+	'guid' => ,
+	'title' => ,
+	'description' => ,
+	'date' => , // timestamp
+	'tags' => , // flickr tags, stored as meta
+);
+*/
+
+	if (!count($photos)) {
+		return;
+	}
+	global $wpdb;
+// grab item hashes, look for existing items (use GUID)
+	$guids = array_keys($photos);
+	$existing = $wpdb->get_col("
+		SELECT guid
+		FROM $wpdb->posts
+		WHERE guid IN ('".implode("', '", array_map(array($wpdb, 'escape'), $guids))."')
+	");
+	if (!AKV3_FLICKR_DRYRUN) {
+		foreach ($existing as $guid) {
+			unset($photos[$guid]);
 		}
 	}
 	if (!count($photos)) {
 		return;
 	}
 
+	$tz = date_default_timezone_get();
+	date_default_timezone_set(AKV3_FLICKR_TIMEZONE);
+
 // create new posts
 	include_once(ABSPATH.'/wp-admin/includes/file.php');
 	include_once(ABSPATH.'/wp-admin/includes/image.php');
 	include_once(ABSPATH.'/wp-admin/includes/media.php');
 	foreach ($photos as $guid => $photo) {
-		$date = strtotime($photo->published);
 		// hack to not bring in older photos
-		if ($date < strtotime('2011-08-14 00:00:00')) {
+		if ($photo['date'] < strtotime('2011-08-14 00:00:00')) {
+
 			continue;
-		}
-		// get the large size
-		$source = str_replace('_m.jpg', '_b.jpg', $photo->media->m);
-		$title = $photo->title;
-		if (empty($title)) {
-			$title = 'Untitled Photo';
 		}
 
 		if (AKV3_FLICKR_DRYRUN) {
-			echo '<pre>';
-			print_r(array(
-				'url' => $source,
-				'guid' => $guid,
-				'post_status' => 'publish',
-				'post_author' => 1,
-				'post_category' => array(AKV3_FLICKR_CAT),
-				'post_title' => $title,
-				'post_date' => date('Y-m-d H:i:s', $date),
-				'cats' => $photo->tags,
-			));
-			echo '</pre>';
+			echo '<pre>'.print_r($photo, true).'</pre>';
 			continue;
 		}
 
@@ -109,21 +148,21 @@ function akv3_flickr_process_feed($url) {
 			'post_status' => 'draft',
 			'post_author' => 1,
 			'post_category' => array(AKV3_FLICKR_CAT),
-			'post_title' => $title,
-			'post_date' => date('Y-m-d H:i:s', $date)
+			'post_title' => $photo['title'],
+			'post_date' => date('Y-m-d H:i:s', $photo['date'])
 		));
 		set_post_format($post_id, 'image');
-		update_post_meta($post_id, '_flickr_tags', $photo->tags);
+		update_post_meta($post_id, '_flickr_tags', $photo['tags']);
 
 // sideload image
 		$file_array = array();
 
 		// Download file to temp location
-		$tmp = download_url($source);
+		$tmp = download_url($photo['url']);
 
 		// Set variables for storage
 		// fix file filename for query strings
-		preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $source, $matches);
+		preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $photo['url'], $matches);
 		$file_array['name'] = basename($matches[0]);
 		$file_array['tmp_name'] = $tmp;
 
